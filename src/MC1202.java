63bd92fd0b4104d828ee8545422488025c6055af
nosefish <nosefish@gmx.net>
2012-11-12 14:48:35 +0100

    Making mc1202 an empty subclass of mcx202 - it's the same thing and only here for compatibility

--- src/MC1202.java
+++ src/MC1202.java
@@ -17,123 +17,13 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import com.sk89q.craftbook.*;
-import com.sk89q.craftbook.ic.*;
 
 /**
- * Dispenser.
+ * Chest Dispenser. Only here for compatibility with craftbook,
+ * which has copied mcx202 from cbx.
  *
  * @author sk89q
  */
-public class MC1202 extends BaseIC {
-    /**
-     * Get the title of the IC.
-     *
-     * @return
-     */
-	@Override
-    public String getTitle() {
-        return "CHEST DISPENSER";
-    }
-
-    /**
-     * Returns true if this IC requires permission to use.
-     *
-     * @return
-     */
-	@Override
-    public boolean requiresPermission() {
-        return true;
-    }
-
-    /**
-     * Validates the IC's environment. The position of the sign is given.
-     * Return a string in order to state an error message and deny
-     * creation, otherwise return null to allow.
-     *
-     * @param sign
-     * @return
-     */
-	@Override
-    public String validateEnvironment(CraftBookWorld cbworld, Vector pos, SignText sign) {
-        String id = sign.getLine3();
-
-        if (id.length() == 0) {
-            return "Specify a item type on the third line.";
-        } else if (getItem(id) < 1) {
-            return "Not a valid item type: " + sign.getLine3() + ".";
-        }
-
-        if (sign.getLine4().length() > 0) {
-            try {
-                Math.min(64, Math.max(1, Integer.parseInt(sign.getLine4())));
-            } catch (NumberFormatException e) {
-                return "Not a valid quantity: " + sign.getLine4() + ".";
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Get an item from its name or ID.
-     * 
-     * @param id
-     * @return
-     */
-    private int getItem(String id) {
-        try {
-            return Integer.parseInt(id.trim());
-        } catch (NumberFormatException e) {
-            return etc.getDataSource().getItem(id.trim());
-        }
-    }
-
-    /**
-     * Think.
-     *
-     * @param chip
-     */
-    @Override
-    public void think(ChipState chip) {
-        if (!chip.getIn(1).is()) {
-            return;
-        }
-        
-        NearbyChestBlockBag source = new NearbyChestBlockBag(chip.getCBWorld(), chip.getPosition());
-        source.addSourcePosition(chip.getCBWorld(), chip.getPosition());
-        
-        String id = chip.getText().getLine3();
-        int quantity = 1;
-
-        try {
-            quantity = Math.min(64,
-                    Math.max(1, Integer.parseInt(chip.getText().getLine4())));
-        } catch (NumberFormatException e) {
-        }
-
-        int item = getItem(id);
-
-        if (item > 0) {
-            Vector pos = chip.getBlockPosition();
-            int maxY = Math.min(CraftBook.MAP_BLOCK_HEIGHT, pos.getBlockY() + 10);
-            int x = pos.getBlockX();
-            int z = pos.getBlockZ();
-
-            World world = CraftBook.getWorld(chip.getCBWorld());
-            for (int y = pos.getBlockY() + 1; y <= maxY; y++) {
-                if (BlockType.canPassThrough(CraftBook.getBlockID(world, x, y, z))) {
-                    int n = 0;
-                    for(n=0;n<quantity;n++)
-                        try {
-                            source.fetchBlock(item);
-                        } catch (BlockSourceException e) {
-                            break;
-                        }
-                    if(n!=0) world.dropItem(x, y, z, item, n);
-                    return;
-                }
-            }
-        }
-    }
+public class MC1202 extends MCX202 {
+	//compatibility
 }
