1dc3c554de1663aa7526bdac3385fe862867618c
nosefish <nosefish@gmx.net>
2013-01-29 14:30:26 +0100

    added workaround for Canary bug #109

--- src/OutputLever.java
+++ src/OutputLever.java
@@ -3,8 +3,9 @@ import java.util.concurrent.LinkedBlockingQueue;
 import com.sk89q.craftbook.WorldBlockVector;
 
 
-public class OutputLever implements Runnable 
-{
+public class OutputLever implements Runnable {
+	
+	final static Player fakePlayer = new Player(); // workaround for Canary bug #109
 	private final LinkedBlockingQueue<WorldBlockVector> outputQueue = new LinkedBlockingQueue<WorldBlockVector>();
 	
 	protected void addToOutputQueue(WorldBlockVector outputBlock) {
@@ -17,12 +18,16 @@ public class OutputLever implements Runnable
 	
 	
 	@Override
-	public void run() {
-		//toggle all levers in the outputQueue with a simulated right-click
+	public void run() {	
+		//toggles all levers in the outputQueue with a simulated right-click
     	for(WorldBlockVector output = outputQueue.poll(); output != null; output = outputQueue.poll()) {
+    		try {
     		CraftBook.getWorld(output.getCBWorld())
     				 .getBlockAt(output.getBlockX(), output.getBlockY(), output.getBlockZ())
-    				 .rightClick(null);    		
+    				 .rightClick(fakePlayer);
+    		} catch (NullPointerException e) {
+    			e.printStackTrace();
+    		}
     	}
     }
 }
