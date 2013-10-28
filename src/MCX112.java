9e21d59cc8db3402468921b0a6d4062722c7c837
Willem Mulder <willemmaster@hotmail.com>
2013-10-28 11:19:10 +0100

    Fix MCX112 formatting

--- src/MCX112.java
+++ src/MCX112.java
@@ -29,19 +29,19 @@ import com.sk89q.craftbook.ic.ChipState;
  * @author sk89q
  */
 public class MCX112 extends BaseIC {
-    
+
 
     /**
      * Get the title of the IC.
      *
      * @return
      */
-	@Override
+    @Override
     public String getTitle() {
         return "TRANSPORTER";
     }
-    
-	@Override
+
+    @Override
     public boolean requiresPermission() {
         return true;
     }
@@ -54,7 +54,7 @@ public class MCX112 extends BaseIC {
      * @param sign
      * @return
      */
-	@Override
+    @Override
     public String validateEnvironment(CraftBookWorld cbworld, Vector pos, SignText sign) {
         String id = sign.getLine3();
 
@@ -70,133 +70,133 @@ public class MCX112 extends BaseIC {
      *
      * @param chip
      */
-	@Override
+    @Override
     public void think(ChipState chip) {
         String id = chip.getText().getLine3();
 
         if (!id.isEmpty() && chip.getIn(1).is()) {
             WorldLocation dest = MCX113.airwaves.get(id);
-            
+
             if (dest == null) {
                 chip.getOut(1).set(false);
             } else
             {
-            	dest = dest.add(0.5D, 0.0D, 0.5D);
-            	
-            	String[] msg;
-        		if(chip.getText().getLine4().length() == 0)
-        			msg = new String[]{"Woosh!"};
-        		else
-        			msg = new String[]{chip.getText().getLine4()};
-        		
-        		chip.getOut(1).set(transport(chip, dest, true, msg));
+                dest = dest.add(0.5D, 0.0D, 0.5D);
+
+                String[] msg;
+                if(chip.getText().getLine4().length() == 0)
+                    msg = new String[]{"Woosh!"};
+                else
+                    msg = new String[]{chip.getText().getLine4()};
+
+                chip.getOut(1).set(transport(chip, dest, true, msg));
             }
         } else {
             chip.getOut(1).set(false);
         }
     }
-    
+
     protected boolean transport(ChipState chip, WorldLocation dest, boolean useSafeY, String[] messages)
     {
-    	if(dest == null)
-    		return false;
-    	
-    	Vector pos;
-    	World world = CraftBook.getWorld(chip.getCBWorld());
-    	
-    	if(chip.getMode() == 'p' || chip.getMode() == 'P')
-    	{
-    		pos = Util.getWallSignBack(world, chip.getPosition(), -2);
-    		if (pos == null) return false;
-    		double newY = pos.getY() + 2;
-    		
-    		if(newY > CraftBook.MAP_BLOCK_HEIGHT)
-    			newY = CraftBook.MAP_BLOCK_HEIGHT;
-    		
-    		pos.setY(newY);
-    	}
-    	else
-    		pos = chip.getBlockPosition();
-    	
+        if(dest == null)
+            return false;
+
+        Vector pos;
+        World world = CraftBook.getWorld(chip.getCBWorld());
+
+        if(chip.getMode() == 'p' || chip.getMode() == 'P')
+        {
+            pos = Util.getWallSignBack(world, chip.getPosition(), -2);
+            if (pos == null) return false;
+            double newY = pos.getY() + 2;
+
+            if(newY > CraftBook.MAP_BLOCK_HEIGHT)
+                newY = CraftBook.MAP_BLOCK_HEIGHT;
+
+            pos.setY(newY);
+        }
+        else
+            pos = chip.getBlockPosition();
+
         int x = pos.getBlockX();
         int z = pos.getBlockZ();
-        
+
         int y = getSafeY(world, pos);
-        
+
         for(Player player: etc.getServer().getPlayerList())
         {
-        	Location pLoc = player.getLocation();
-        	Vector pVec = new Vector(pLoc.x, pLoc.y, pLoc.z);
-        	
-        	if(player.getWorld() == world
-        	   && (pVec.getBlockX() == x || pVec.getBlockX() == x + 1 || pVec.getBlockX() == x - 1)
-        	   &&  pVec.getBlockY() == y
-        	   && (pVec.getBlockZ() == z || pVec.getBlockZ() == z + 1 || pVec.getBlockZ() == z - 1)
-        		)
-        	{
-        		if(useSafeY)
-        		{
-        			dest = dest.setY(getSafeY(CraftBook.getWorld(dest.getCBWorld()), dest.getCoordinate()) + 1.0D);
-        		}
-                
+            Location pLoc = player.getLocation();
+            Vector pVec = new Vector(pLoc.x, pLoc.y, pLoc.z);
+
+            if(player.getWorld() == world
+               && (pVec.getBlockX() == x || pVec.getBlockX() == x + 1 || pVec.getBlockX() == x - 1)
+               &&  pVec.getBlockY() == y
+               && (pVec.getBlockZ() == z || pVec.getBlockZ() == z + 1 || pVec.getBlockZ() == z - 1)
+                )
+            {
+                if(useSafeY)
+                {
+                    dest = dest.setY(getSafeY(CraftBook.getWorld(dest.getCBWorld()), dest.getCoordinate()) + 1.0D);
+                }
+
                 if(messages != null)
                 {
-                	for(String message : messages)
-                	{
-                		if(message == null)
-                			break;
-                		player.sendMessage(Colors.Gold+message);
-                	}
+                    for(String message : messages)
+                    {
+                        if(message == null)
+                            break;
+                        player.sendMessage(Colors.Gold+message);
+                    }
                 }
-                
-        		//player.teleportTo(dest);
-        		CraftBook.teleportPlayer(player, dest);
-        		
-        		if(chip.getMode() == 'P')
-        		{
-        			//force plate off
-            		int bdata = CraftBook.getBlockID(world, pVec);
-            		if(bdata == BlockType.STONE_PRESSURE_PLATE || bdata == BlockType.WOODEN_PRESSURE_PLATE)
-            		{
-            			OWorld oworld = player.getWorld().getWorld();
-            			
-            			int bx = pVec.getBlockX();
-            			int by = pVec.getBlockY();
-            			int bz = pVec.getBlockZ();
-            			
-            			oworld.c(bx, by, bz, 0);
-            			//func_72898_h
-            			oworld.f(bx, by, bz, bdata);
-            			oworld.f(bx, by - 1, bz, bdata);
-            			oworld.e(bx, by, bz, bx, by, bz);
-            		}
-        		}
-        		
-        		return true;
-        	}
+
+                //player.teleportTo(dest);
+                CraftBook.teleportPlayer(player, dest);
+
+                if(chip.getMode() == 'P')
+                {
+                    //force plate off
+                    int bdata = CraftBook.getBlockID(world, pVec);
+                    if(bdata == BlockType.STONE_PRESSURE_PLATE || bdata == BlockType.WOODEN_PRESSURE_PLATE)
+                    {
+                        OWorld oworld = player.getWorld().getWorld();
+
+                        int bx = pVec.getBlockX();
+                        int by = pVec.getBlockY();
+                        int bz = pVec.getBlockZ();
+
+                        oworld.c(bx, by, bz, 0);
+                        //func_72898_h
+                        oworld.f(bx, by, bz, bdata);
+                        oworld.f(bx, by - 1, bz, bdata);
+                        oworld.e(bx, by, bz, bx, by, bz);
+                    }
+                }
+
+                return true;
+            }
         }
-    	
+
         return false;
     }
-    
+
     @Deprecated
     protected static int getSafeY(World world, Vector pos)
     {
-//    	int maxY = Math.min(CraftBook.MAP_BLOCK_HEIGHT, pos.getBlockY() + 10);
+//      int maxY = Math.min(CraftBook.MAP_BLOCK_HEIGHT, pos.getBlockY() + 10);
 //        int x = pos.getBlockX();
 //        int z = pos.getBlockZ();
-//    	
-//    	for (int y = pos.getBlockY() + 1; y <= maxY; y++)
-//		{
+//
+//      for (int y = pos.getBlockY() + 1; y <= maxY; y++)
+//      {
 //            if (BlockType.canPassThrough(CraftBook.getBlockID(world, x, y, z)) &&
-//            	y < CraftBook.MAP_BLOCK_HEIGHT && BlockType.canPassThrough(CraftBook.getBlockID(world, x, y+1, z))	
-//            	)
+//              y < CraftBook.MAP_BLOCK_HEIGHT && BlockType.canPassThrough(CraftBook.getBlockID(world, x, y+1, z))
+//              )
 //            {
-//            	return y;
+//              return y;
 //            }
-//		}
-//    	
-//    	return maxY;
-    	return Util.getSafeYAbove(world, pos);
+//      }
+//
+//      return maxY;
+        return Util.getSafeYAbove(world, pos);
     }
 }
